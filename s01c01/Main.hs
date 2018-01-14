8da7c0408b869de1c71e8c82e2a7bdc9157ed540
Richard Cook <rcook@rcook.org>
2018-01-14 12:06:36 -0800

    Fix whitespace

--- s01c01/Main.hs
+++ s01c01/Main.hs
@@ -6,6 +6,7 @@ import Imports
 main :: IO ()
 main = do
     putStrLn "Set 1 challenge 1"
+
     let hs = fromJust $ hexString "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     print hs
     print $ (base64Encode . hexDecode) hs
--- s01c02/Main.hs
+++ s01c02/Main.hs
@@ -6,6 +6,7 @@ import Imports
 main :: IO ()
 main = do
     putStrLn "Set 1 challenge 2"
+
     let s0 = fromJust $ hexString "1c0111001f010100061a024b53535009181c"
         s1 = fromJust $ hexString "686974207468652062756c6c277320657965"
     print s0
