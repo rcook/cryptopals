dd8778755e1878a8fae1d08c1700cd5da179f3a5
Richard Cook <rcook@rcook.org>
2018-01-18 08:52:04 -0800

    Tidy up

--- challenge12/Main.hs
+++ challenge12/Main.hs
@@ -18,12 +18,6 @@ unknownPlaintextPadding =
             \dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg\
             \YnkK"
 
---unknownPlaintextPadding = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only."
-
-
-
-
-
 main :: IO ()
 main = do
     putStrLn "challenge12"
@@ -31,7 +25,6 @@ main = do
     g <- newStdGen
     let (unknownKey, _) = newAESKey g
         encryptor s = encryptECB unknownKey (ByteString.append s unknownPlaintextPadding)
-        --plaintext = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only."
 
     let (chunkSize, isECBResult, plaintext) = breakEBC encryptor
     putStrLn $ "Chunk size      : " ++ show chunkSize
